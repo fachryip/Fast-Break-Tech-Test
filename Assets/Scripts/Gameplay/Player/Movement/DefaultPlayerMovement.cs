fec92f50deecb30d27df92401d062682556ee5e4
Fachry <fachry@confiction.com>
2025-08-05 05:34:50 +0700

    basic player movement

--- Assets/Scripts/Gameplay/Player/Movement/DefaultPlayerMovement.cs
+++ Assets/Scripts/Gameplay/Player/Movement/DefaultPlayerMovement.cs
@@ -7,6 +7,23 @@ public class DefaultPlayerMovement : BasePlayerMovement
 {
     public override void Move(PlayerController player, Vector2 moveInput)
     {
-        var moveValue = moveInput * MovementSpeed;
+        if (moveInput == Vector2.zero) return;
+
+        Movement(player, moveInput);
+        Rotation(player, moveInput);
+    }
+
+    private void Movement(PlayerController player, Vector2 moveInput)
+    {
+        var value = moveInput * MovementSpeed * Time.deltaTime;
+        var direction = new Vector3(value.x, player.transform.position.y, value.y);
+        player.transform.position += direction;
+    }
+
+    private void Rotation(PlayerController player, Vector2 moveInput)
+    {
+        var speed = RotationSpeed * Time.deltaTime;
+        var moveForward = new Vector3(moveInput.x, 0f, moveInput.y);
+        player.transform.forward = Vector3.Lerp(player.transform.forward, moveForward, speed);
     }
 }
